179829bfa103adbbbd0454c1f04a98c3d317d08b
Lina <linabell@mail.com>
2026-08-07 13:17:31 -0700

    copy update

--- client/src/components/AdminView.tsx
+++ client/src/components/AdminView.tsx
@@ -72,9 +72,9 @@ export const AdminView = () => {
       <div>
         <label className="label">Lives</label>
         <select className="input p2" value={lives} onChange={(e) => setLives(Number(e.target.value))}>
-          <option value={0}>Sudden Death</option>
+          <option value={0}>1 Life</option>
           <option value={3}>3 Lives</option>
-          <option value={5}>5 Life</option>
+          <option value={5}>5 Lives</option>
         </select>
       </div>
 
